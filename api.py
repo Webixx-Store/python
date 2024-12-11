8a04188142b301b013fe277c503a3231add05e70
Webixx-Store <contact.webixx@gmail.com>
2024-12-11 15:53:49 +0700

    a

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
+import requests
+import time
+import threading
 from binance_client import analyze_signals, get_historical_data
 from binance.client import Client
-from flask import Flask, request, jsonify
-
+from flask import Flask, jsonify
 
 # Constants
 SYMBOL = "BTCUSDT"
@@ -12,13 +14,25 @@ STOP_LOSS = 300
 
 app = Flask(__name__)
 
+# Hàm gửi yêu cầu GET đến API của bạn
+def call_api():
+    url = "https://python-fk3x.onrender.com"  # Thay đổi với URL thực tế của bạn
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print("Yêu cầu thành công!")
+                print("Nội dung phản hồi:")
+                print(response.text)
+            else:
+                print(f"Yêu cầu thất bại. Mã trạng thái: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"Đã có lỗi xảy ra: {e}")
+        
+        time.sleep(30)  # Đợi 30 giây trước khi gọi lại
+
 @app.route('/analyze', methods=['GET'])
 def analyze():
-    # Get query parameters
-    symbol = 'BTCUSDT'
-    # interval = request.args.get('interval', '1D')
-    # limit = int(request.args.get('limit', 100))
-    
     try:
         # Get historical data
         data = get_historical_data(symbol=SYMBOL, interval=INTERVAL, limit=LIMIT)
@@ -31,4 +45,8 @@ def analyze():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
+    # Chạy bot trong một luồng riêng biệt
+    threading.Thread(target=call_api, daemon=True).start()
+
+    # Chạy Flask app
     app.run(host='0.0.0.0', port=5000)
